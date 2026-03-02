8fd14ec21c14fa5bfb6aeaeb6cf2c36bcf03d714
Your Name <corey.robinson@greenwaydevelopment.net>
2026-03-02 01:18:12 -0500

    create player

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 VERSION = pygame.version.ver
@@ -15,15 +16,26 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     running = True
 
+    game_clock = pygame.time.Clock()    # game_clock
+    dt = 0
+    my_player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while running:
-        log_state()
+        log_state() #Start logging to game_state.jsonl
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("purple")
+        screen.fill("black")
+
+        my_player.draw(screen)
 
         pygame.display.flip()
-
+        
+        # game_clock.tick(60)     # pause the game loop until 1/60th of a second has passed
+        dt = (game_clock.tick(60)) /1000
+        
+        
 
 if __name__ == "__main__":
     main()
